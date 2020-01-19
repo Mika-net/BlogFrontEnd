c9700b96bed66e6133d73cc8baa4ecd6593f1580
Mika <johnson.michael.mn@gmail.com>
2020-01-19 17:26:18 -0600

    clean house, purge

--- src/components/organisms/BioShort/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { BioShort } from 'components'
-
-storiesOf('BioShort', module)
-  .add('default', () => (
-    <BioShort/>
-  ))
