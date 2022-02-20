24b738123d13abd34c0cad96bb7637054c1696d6
Mika <mika@mika-net.io>
2022-02-20 17:43:17 -0600

    change links to github

--- src/components/organisms/ProjectsList/index.js
+++ src/components/organisms/ProjectsList/index.js
@@ -21,32 +21,28 @@ const ProjectsList = (props) => {
       <Grid>
         <Card
           title="Python Projects"
-          link="https://www.python.org/"
+          link="https://github.com/Mika-net?tab=repositories&q=&type=&language=python&sort="
           image="https://images.unsplash.com/photo-1528158222524-d4d912d2e208?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&h=600&q=80"
         >
           A collection of all the python projects I've worked on and been a part of!
         </Card>
         <Card
           title="Front End Projects"
-          link="https://reactjs.org/"
+          link="https://github.com/Mika-net?tab=repositories&q=&type=&language=javascript&sort="
           image="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&h=600&q=80"
         >
           React, Anglur and static designs, I've done a few different projects using each!
         </Card>
         <Card
           title="Rust Projects"
-          link="https://www.rust-lang.org/"
+          link="https://github.com/Mika-net?tab=repositories&q=&type=&language=rust&sort="
           image="https://images.unsplash.com/photo-1416424312427-baefa7707d85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&h=600&q=60"
-        >
-
-        </Card>
+        />
         <Card
           title="C# Projects"
-          link="https://docs.microsoft.com/en-us/dotnet/csharp/index"
+          link="https://github.com/Mika-net?tab=repositories&q=&type=&language=c%23&sort="
           image="https://images.unsplash.com/photo-1505683040473-85e66a1ca54d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&h=600&q=80"
-        >
-
-        </Card>
+        />
       </Grid>
     </div>
   )
